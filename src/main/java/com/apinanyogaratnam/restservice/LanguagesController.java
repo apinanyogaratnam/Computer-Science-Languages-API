8e50b61a47061a2d1a7820c6ce5ad478b925c311
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-18 16:57:13 -0400

    removed unused imports

--- src/main/java/com/apinanyogaratnam/restservice/LanguagesController.java
+++ src/main/java/com/apinanyogaratnam/restservice/LanguagesController.java
@@ -2,10 +2,6 @@ package com.apinanyogaratnam.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicLong;
 
