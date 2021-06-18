fdc040652e5db46e17035ced57c32f0ada982f20
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-18 16:01:51 -0400

    init repo with constructor added and contoller class created

---
+++ src/main/java/com/apinanyogaratnam/restservice/Languages.java
@@ -0,0 +1,28 @@
+package com.apinanyogaratnam.restservice;
+
+public class Languages {
+
+    private String name;
+    private int year;
+
+    public Languages(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/com/apinanyogaratnam/restservice/LanguagesController.java
@@ -0,0 +1,9 @@
+package com.apinanyogaratnam.restservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LanguagesController {
+
+
+}
